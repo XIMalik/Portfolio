3506820f194dadfbd279a2fe0f4fde802cadb1f7
XIMalik <abdulmalikawesu@gmail.com>
2023-02-09 11:53:48 +0100

    tt

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,7 +13,6 @@ export default function Form(){
 
             <form 
             action="post" 
-            data-netlify="true" 
             name="contact v1" 
             onSubmit="submit"
 
@@ -48,10 +47,6 @@ export default function Form(){
                 <div className="col-span-2 space-y-2 field">
                     <label htmlFor="inquiry" className="text-gray-600">Drop an inquiry</label>
                     <textarea placeholder="Tell me what you have in mind" name="inquiry" id="inquiry" cols="30" rows="5" className="block placeholder-gray-800 focus:outline-none w-full col-span-2 rounded-lg bg-[#201f1f] p-5"></textarea>
-                </div>
-                
-                <div className="field">
-                    <div data-netlify-recaptcha="true"></div>
                 </div>
                 
                 <button className="text-center col-span-2 bg-slate-900 pt-3 pb-3 rounded-md transition-colors ease-in-out delay-300 duration-300 hover:bg-gradient-to-r from-slate-900 to-pink-500 hover:cursor-pointer" type="submit">Submit</button>
