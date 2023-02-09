bb0ed20fb6087ff01e2f75030efbf7552c5bfd2c
XIMalik <abdulmalikawesu@gmail.com>
2023-02-09 11:07:19 +0100

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -31,7 +31,7 @@ export default function Form(){
             <div className="hidden lg:block bg-[#161313] p-10 md:h-screen">
                 <img src={pic2} alt="" className=" mx-auto md:block object-cover h-[100%] w-[100%] hidden"/>
             </div>
-            <form action="POST" className="bg-[url('/assets/pic2.png')] grid grid-cols-2 w-fit mx-auto gap-5 p-4" data-netlify="true" name="contact">
+            <form action="POST" data-netlify="true" className="bg-[url('/assets/pic2.png')] grid grid-cols-2 w-fit mx-auto gap-5 p-4">
                 <div className="space-y-2 col-span-2 ">
                     <h1 className="text-2xl md:text-5xl">Get in touch</h1>
                     <p className="text-slate-400">Lets work together on your project</p>
@@ -39,25 +39,27 @@ export default function Form(){
 
                 <div className="gap-5 flex justify-center col-span-2">
                     <div className="w-1/2 space-y-2">
-                        <label htmlFor="firstname" className="text-left text-gray-600">Name</label>
-                        <input className="pb-3 pt-3 pl-5 text-zinc-300 block w-full rounded focus:outline-none focus:shadow-md focus:border-10 shadow-slate-900 bg-[#201f1f] placeholder-gray-800" type="text" name="firstname" placeholder="Johnny Depp" />
+                        <label htmlFor="name" className="text-left text-gray-600">Name</label>
+                        <input type="text" name="name" id="name" placeholder="Johnny Depp" className="pb-3 pt-3 pl-5 text-zinc-300 block w-full rounded focus:outline-none focus:shadow-md focus:border-10 shadow-slate-900 bg-[#201f1f] placeholder-gray-800" />
                     </div>
                     <div className="w-1/2 space-y-2">
-                        <label htmlFor="firstname" className="text-left text-gray-600">Job</label>
-                        <input className="pb-3 pt-3 pl-5 text-zinc-300 block px-4 py-2 w-full rounded placeholder-gray-800 focus:outline-none focus:shadow-md focus:border-10 focus:border-red-500 shadow-slate-900 bg-[#201f1f]" type="text" name="job" placeholder="UI Design" />
+                        <label htmlFor="name" className="text-left text-gray-600">Job</label>
+                        <input type="text" name="job" id="job" placeholder="UI Design" className="pb-3 pt-3 pl-5 text-zinc-300 block px-4 py-2 w-full rounded placeholder-gray-800 focus:outline-none focus:shadow-md focus:border-10 focus:border-red-500 shadow-slate-900 bg-[#201f1f]"  />
                     </div>
 
                 </div>
                 <div className="col-span-2 space-y-2">
                     <label htmlFor="mail" className="text-gray-600">Email address</label>
-                    <input className="col-span-2 pb-3 pt-3 pl-5 text-zinc-300 block px-4 py-2 w-full rounded placeholder-gray-800 focus:outline-none focus:shadow-md focus:border-1 focus:border-red-500 shadow-slate-900 bg-[#201f1f]" type="text" name="email" placeholder="johndoe@gmail.com" />
+                    <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" className="col-span-2 pb-3 pt-3 pl-5 text-zinc-300 block px-4 py-2 w-full rounded placeholder-gray-800 focus:outline-none focus:shadow-md focus:border-1 focus:border-red-500 shadow-slate-900 bg-[#201f1f]" />
                 </div>
                 <div className="col-span-2 space-y-2">
                     <label htmlFor="mail" className="text-gray-600">Drop an inquiry</label>
-                    <textarea className="block placeholder-gray-800 focus:outline-none w-full col-span-2 rounded-lg bg-[#201f1f] p-5" placeholder="Let me know what you have in mind" name="inquiry" id="" cols="30" rows="5"></textarea>
+                    <textarea placeholder="Tell me what you have in mind" name="inquiry" id="inquiry" cols="30" rows="5" className="block placeholder-gray-800 focus:outline-none w-full col-span-2 rounded-lg bg-[#201f1f] p-5"></textarea>
                 </div>
                 
-                <div className="field" data-netlify-recaptcha="true"></div>
+                <div className="field">
+                    <div data-netlify-recaptcha="true"></div>
+                </div>
                 
                 <input className="text-center col-span-2 bg-slate-900 pt-3 pb-3 rounded-md transition-colors ease-in-out delay-300 duration-300 hover:bg-gradient-to-r from-slate-900 to-pink-500 hover:cursor-pointer" type="submit"/>
                 {/* <div className="col-span-2 text-block text-center space-y-2">
